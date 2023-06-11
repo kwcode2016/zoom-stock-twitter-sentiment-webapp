ed041ddf96882c1d27ea8478284f6f9e66810164
Ken Wee <kwhome2002@gmail.com>
2023-06-11 11:28:38 -0700

    updating webapp

--- webapp-streamlit-twitter-zoom.py
+++ webapp-streamlit-twitter-zoom.py
@@ -5,25 +5,26 @@ import datetime
 
 
 
-
-
 st.set_page_config(page_title='Zoom Stock Twitter Analysis')
-
-
 
 
 default_start_date = datetime.date(2019, 4, 1)
 default_end_date = datetime.date(2022, 12, 30)
 
+st.sidebar.markdown('## Instructions')
+st.sidebar.markdown("Enter new dates in the Start Date and End Date fields and the chart will automatically update.")
+st.sidebar.markdown("Please note that the dates must range between April 1 2019 and Dec 30 2022")
+st.sidebar.markdown("---")
 start_date = st.sidebar.date_input("Start Date", default_start_date)
 end_date = st.sidebar.date_input("End Date", default_end_date)
 
 
-st.title("Zoom Stock Prices with Twitter Sentiment Analysis")
 
+st.title("Zoom Twitter Sentiment vs Stock Prices Analysis")
 
-st.write("Stock Price Chart")
-
+st.markdown('---')
+st.markdown('## Seperated Stock Price and Twitter Daily Count Charts')
+st.markdown('Use these charts to spot interesting date ranges to explore.')
 
 
 @st.cache_data
@@ -45,20 +46,6 @@ print(zm_date_close.info())
 zm_date_close = zm_date_close[(zm_date_close['Date'] >= pd.to_datetime(start_date)) & (zm_date_close['Date'] <= pd.to_datetime(end_date))]
 
 
-
-zm_price_chart = alt.Chart(zm_date_close).mark_line().encode(
-    x='Date',
-    y='Close'
-).properties( 
-    height=400,
-    width=800
-    )
-
-
-# zm_price_chart
-
-
-
 # using matplotlib
 import matplotlib.pyplot as plt
 
@@ -75,6 +62,7 @@ plt.xlabel('Date')
 
 # Set the y-axis label
 plt.ylabel('Price')
+plt.xticks(rotation=45, ha='right')
 
 # Display the chart
 st.pyplot(plt)
@@ -91,48 +79,6 @@ zm_tweet_sentiment_df = zm_tweet_sentiment_df[(zm_tweet_sentiment_df['date'] >= 
 daily_sum = zm_tweet_sentiment_df.groupby([zm_tweet_sentiment_df['date'].dt.date, 'sentiment_r_latest'])['sentiment_r_latest'].count()
 daily_sum = daily_sum.unstack(fill_value=0)
 daily_sum.reset_index(level=0, inplace=True) # date column was not flat and cannot be accessed - this flattens all columns
-
-import altair as alt
-import pandas as pd
-
-
-# Create an Altair Chart from the DataFrame
-zm_positive_chart = alt.Chart(daily_sum).mark_line(color='green').encode(
-    # x='yearmonth(date):O',
-    x='date',
-    # y='sum(positive)',
-    y='positive',
-).properties(
-    width=800,
-    height=400
-)
-
-
-zm_negative_chart = alt.Chart(daily_sum).mark_line(color='red').encode(
-    x='date',
-    # x='yearmonth(date):O',
-    # y='negative',
-    # y='sum(negative)',
-    y='negative',
-).properties(
-    width=800,
-    height=400
-)
-
-zm_neutral_chart = alt.Chart(daily_sum).mark_line(color='grey').encode(
-    x='date',
-    # x='yearmonth(date):O',
-    # y='neutral',
-    # y='sum(neutral)',
-    y='neutral',
-).properties(
-    width=800,
-    height=400
-)
-
-
-# zm_positive_chart + zm_negative_chart + zm_neutral_chart
-
 
 
 
@@ -158,16 +104,19 @@ plt.plot(daily_sum['date'], daily_sum['negative'], label='negative', color='red'
 plt.legend()
 
 # Set the title and labels
-plt.title("Sentiment Scores over Time")
+plt.title("Daily Positive and Negative Sentiment Counts over Time")
 plt.xlabel("Date")
-plt.ylabel("Num of Tweets")
+plt.xticks(rotation=45, ha='right')
+plt.ylabel("Daily Sentiment Counts")
 
 st.pyplot(plt)
 
 
 
 
-
+st.markdown('---')
+st.header('Combined Zoom Sentiment Stock Price Graph')
+st.markdown('Analyze the data using the graph with the stock price overlayed with the daily Twitter sentiment counts.')
 
 # trying to combine both charts
 
@@ -198,11 +147,14 @@ ax1.legend(loc='upper left')
 ax2.legend(loc='upper right')
 
 # Set the title and labels
-ax1.set_title("Sentiment Scores and Price over Time")
+ax1.set_title("Daily Positive and Negative Twitter Sentiment + Price over Time")
 ax1.set_xlabel("Date")
-ax1.set_ylabel("Score")
+
+ax1.set_ylabel("Daily Sentiment Counts")
 ax2.set_ylabel("Price")
 
+
+plt.xticks(rotation=45, ha='right')
 
 st.pyplot(plt)
 
